bdc7c5acc43056fa6901d3d009fab239cf0247a0
elabios <elabios@HESTIA>
2015-11-06 15:52:26 +0800

    first commit

---
+++ Test3/src/test3pckage/Test3Class.java
@@ -0,0 +1,23 @@
+package test3pckage;
+
+import java.util.Scanner;
+
+public class Test3Class {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("First input number: ");
+		Scanner scan = new Scanner(System.in);
+		int a = scan.nextInt();
+		System.out.println("Second input number: ");
+		int b = scan.nextInt();
+		int c = a+b;
+		System.out.println("Sum = "+ c);
+		
+		
+	}
+
+}
