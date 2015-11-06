df0f355a09398191f6bf7ee1f7c841396c323f1f
elabios <elabios@HESTIA>
2015-11-06 19:24:23 +0800

    commit

---
+++ Test4/src/test4package/Test4Class.java
@@ -0,0 +1,15 @@
+package test4package;
+
+public class Test4Class {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Test4");
+		
+
+	}
+
+}
