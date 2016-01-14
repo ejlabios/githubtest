23300f31474365e7459bd52d533af975f01d6c05
elabios2 <elabios@HESTIA>
2016-01-14 21:32:01 +0800

    for merging

--- Test3/src/test3pckage/Test3Class.java
+++ Test3/src/test3pckage/Test3Class.java
@@ -15,7 +15,7 @@ public class Test3Class {
 		System.out.println("Second input number: ");
 		int b = scan.nextInt();
 		int c = a+b;
-		int d=a+a;
+		int d=a+a+b;
 		System.out.println("Sum = "+ c);
 		
 		
